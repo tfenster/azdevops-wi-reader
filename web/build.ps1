7217fef762321adfdb8590ac7db00d89f916c87b
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-02-23 21:10:44 +0100

    fix

--- web/build.ps1
+++ web/build.ps1
@@ -1,6 +1,6 @@
 $version = ((Invoke-Expression "git describe --abbrev=0 --tags").Substring(1))
 Invoke-Expression "docker build -t tobiasfenster/azdevops-wi-reader:$($version)-1809 -f Dockerfile.1809 ."
-Invoke-Expression "docker login -u $($env:docker_user) -p $($env:docker_pwd) docker.io"
+#Invoke-Expression "docker login -u $($env:docker_user) -p $($env:docker_pwd) docker.io"
 Invoke-Expression "docker images"
 Write-Host "docker push `"tobiasfenster/azdevops-wi-reader:$($version)-1809`""
 Invoke-Expression "docker push `"tobiasfenster/azdevops-wi-reader:$($version)-1809`""
