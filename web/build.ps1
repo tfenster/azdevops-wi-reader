b2b3cb07740e67f38db85066fe497e114063545c
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-02-23 20:29:46 +0100

    try in script

---
+++ web/build.ps1
@@ -0,0 +1,5 @@
+$version = ((Invoke-Expression "git describe --abbrev=0 --tags").Substring(1))
+Invoke-Expression "docker build -t tobiasfenster/azdevops-wi-reader:$($version)-1809 -f Dockerfile.1809 ."
+Invoke-Expression "docker login -u $($env:docker_user) -p $($env:docker_pwd)"
+Invoke-Expression "docker push `"tobiasfenster/azdevops-wi-reader:$($version)-1809`""
+Invoke-Expression "docker logout"
