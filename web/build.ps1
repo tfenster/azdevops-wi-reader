630357a2383886a91fc0e0b502263d2053246975
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-02-23 21:37:37 +0100

    try other action

--- web/build.ps1
+++ web/build.ps1
@@ -1,9 +1,9 @@
 $version = ((Invoke-Expression "git describe --abbrev=0 --tags").Substring(1))
 Invoke-Expression "docker build -t tobiasfenster/azdevops-wi-reader:$($version)-1809 -f Dockerfile.1809 ."
 Invoke-Expression "docker images"
-Write-Host "docker login -u `"$($env:docker_user)`" -p `"$($env:docker_pwd)`" docker.io"
+#Write-Host "docker login -u `"$($env:docker_user)`" -p `"$($env:docker_pwd)`" docker.io"
 Write-Host "docker push `"tobiasfenster/azdevops-wi-reader:$($version)-1809`""
-echo "$($env:docker_pwd)" | docker login -u "$($env:docker_user)" --password-stdin
-Invoke-Expression "docker login -u `"$($env:docker_user)`" -p `"$($env:docker_pwd)`" docker.io"
+#echo "$($env:docker_pwd)" | docker login -u "$($env:docker_user)" --password-stdin
+#Invoke-Expression "docker login -u `"$($env:docker_user)`" -p `"$($env:docker_pwd)`" docker.io"
 Invoke-Expression "docker push `"tobiasfenster/azdevops-wi-reader:$($version)-1809`""
 Invoke-Expression "docker logout"
